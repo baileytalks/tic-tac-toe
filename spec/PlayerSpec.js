d4fac3010a76b43a174551ff06243d27a27589a4
Bailey Kursar <bkursar@gmail.com>
2017-06-15 10:26:39 +0100

    Added a test for an array of choices for each player and made it pass

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -5,4 +5,8 @@ describe('Player', function() {
   it('instansiating a player does not throw an error', function() {
     expect(player).toBeDefined()
   });
+
+  it('each player has an array to contain their choices', function() {
+    expect(player.choices).toBeDefined()
+  });
 });
