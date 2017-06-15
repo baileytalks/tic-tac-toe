eea215fed93cfffc595b6f9a53a83013312e3d2c
Bailey Kursar <bkursar@gmail.com>
2017-06-15 10:22:19 +0100

    Added a test to see if players are passed into game on instantiation and saved, and made it pass

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,6 +1,6 @@
 describe('Game', function() {
 
-  var game = new Game();
+  var game = new Game('player1', 'player2');
 
   it('instansiating a game does not throw an error', function() {
     expect(game).toBeDefined()
@@ -8,5 +8,9 @@ describe('Game', function() {
 
   it('a new game has an array of turns', function() {
     expect(game.turns).toBeDefined()
+  });
+
+  it('player1 is saved as a variable when passed as an arg', function() {
+    expect(game.player1).toEqual('player1')
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,26 +1,5 @@
-// function Player() {
-// }
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.pause = function() {
-//   this.isPlaying = false;
-// };
-//
-// Player.prototype.resume = function() {
-//   if (this.isPlaying) {
-//     throw new Error("song is already playing");
-//   }
-//
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.makeFavorite = function() {
-//   this.currentlyPlayingSong.persistFavoriteStatus(true);
-// };
-
-function Game() {
+function Game(player1, player2) {
   this.turns = []
+  this.player1 = player1
+  this.player2 = player2
 }
